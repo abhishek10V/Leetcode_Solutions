7b0e37306c5c81884343043d8ede0cd8fe2e6906
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-07-02 11:01:34 +0530

    Time: 4 ms (59.42%), Space: 14.2 MB (21.98%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
-        sort(nums2.begin(),nums2.end());
-        sort(nums1.begin(),nums1.end());
-        int i=0, j=0;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i] == nums2[j]){
-                result.push_back(nums1[i]);
-                i++;
-                j++;
-            }else if(nums1[i] < nums2[j]) i++;
-            else j++;
+          vector<int> res;
+        unordered_map<int, int> box;
+        for (int num : nums1)
+            box[num]++;
+        for (int i = 0; i < nums2.size(); ++i) {
+            if (box[nums2[i]] > 0) {
+                res.push_back(nums2[i]);
+                box[nums2[i]]--;
+            } else {
+                continue;
+            }
         }
-        return result;
+        return res;
     }
 };
