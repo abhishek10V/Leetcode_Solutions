01706e11ed93185a3fe98d236bb7ef515781d61e
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-07-10 14:29:31 +0530

    Time: 1 ms (99.42%), Space: 42 MB (51.62%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,20 +1,16 @@
 class Solution {
     public int minOperations(String[] logs) {
-        Stack<String> st = new Stack<>();
+        int point = 0;
         for(int i=0; i<logs.length; i++){
-            if(logs[i].equals("../") && !st.isEmpty()){
-                st.pop();
-            }else if((logs[i].equals("../") && st.isEmpty()) || logs[i].equals("./")) continue;
-            else {
-                int in = 0;
-                 StringBuilder s = new StringBuilder();
-                while(logs[i].charAt(in) != '/'){
-                    s.append(logs[i].charAt(in));
-                    in++;
-                }
-               st.push(s.toString());
+            if(logs[i].equals("../")){
+                if(point > 0)
+                point--;
+            }
+            else if(logs[i].equals("./"));
+            else{
+               point++;
             }
         } 
-        return st.size(); 
+        return point; 
     }
 }
