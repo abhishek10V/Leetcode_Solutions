476ec7f3c7e7fc2cb49dbbe58f35288350115ea7
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-07-19 10:18:49 +0530

    Time: 2 ms (93.93%), Space: 45.1 MB (66.83%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -1,26 +1,31 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
-        int n = matrix.length , m = matrix[0].length;
-        List<Integer> res = new ArrayList<>();
-
-        for(int i=0 ; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(isRow(matrix[i][j] , matrix, i , m) && isCol(matrix[i][j], matrix , j , n))
-                res.add(matrix[i][j]);
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<matrix.length;i++){
+            int minRow = matrix[i][0];
+            int find  = 0;
+            for(int j=0;j<matrix[i].length;j++){
+                if(minRow>matrix[i][j]){
+                    minRow=matrix[i][j];
+                    find = j;    
+                }
             }
+            int maxCol = minRow;
+            Boolean ans = false;
+            for(int k=0;k<matrix.length;k++){
+                if(maxCol<matrix[k][find]){
+                    ans=true;
+                    break;
+                }
+            }
+            
+            if(ans==true){
+                continue;
+            }
+            else{
+                list.add(maxCol);
+            }   
         }
-        return res;
-    }
-    public boolean isRow(int ele , int[][] mat, int row , int m){
-        for(int j=0 ; j<m ; j++){
-            if(mat[row][j] < ele) return false;
-        }
-        return true;
-    }
-    public boolean isCol(int ele , int[][] mat, int col , int n){
-        for(int i=0 ; i<n ; i++){
-            if(mat[i][col] > ele) return false;
-        }
-        return true;
+        return list;
     }
 }
