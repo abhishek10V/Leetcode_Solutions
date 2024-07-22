b91d5a9c310e5ec8aeee90fdcce97797c2a62919
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-07-22 17:00:49 +0530

    Time: 1 ms (100%), Space: 57.3 MB (35.98%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,16 +1,31 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        HashSet<Integer> set = new HashSet<>();
+        int in = -1;
+       for(int i=0 ; i<nums.length; i++){
+        if(nums[i] != 1) in  = i;
+        else break;
+       }
+       if(in == nums.length - 1) return 1;
+       else{
+         transform(nums);
+         for(int i=0; i<nums.length; i++){
+            int val = Math.abs(nums[i]);
+            int idx = val - 1;
 
-        for(int i=0 ; i<nums.length; i++){
-            set.add(nums[i]);
-        }
+            if(nums[idx] > 0)
+            nums[idx] = -nums[idx];
+         }
 
-        for(int i=1; i<=nums[nums.length - 1]; i++){
-            if(!set.contains(i)) return i;
-        }
-        
-        return (nums[nums.length - 1] <= 0) ? 1 : nums[nums.length - 1] + 1;
+         for(int i=0 ; i<nums.length; i++){
+            if(nums[i] > 0) return i + 1;
+         }
+       }
+       return nums.length + 1;
+    }
+    public void transform(int[] nums){
+       int n = nums.length;
+       for(int i=0 ;i<n ; i++){
+         if(nums[i] <= 0 || nums[i] > n) nums[i] = 1;
+       }
     }
 }
