8c09274029799a861bf3d9284d4206eca576c06b
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-08-14 20:41:33 +0530

    Time: 25 ms (19.71%), Space: 20.9 MB (12.47%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -4,16 +4,14 @@ public:
         if(s.size() == 0) return 0;
         sort(s.begin() , s.end());
         sort(g.begin() , g.end());
-        int n = g.size() - 1;
+        int i = g.size() - 1, j = s.size() - 1;
         int ans = 0;
-        while(s.size() != 0 && n >= 0 && g.size() != 0){
-            if(s[s.size() - 1] >= g[n]){
-                ans++;
-                s.pop_back();
-                g.erase(g.begin() + n);
-            }else{
-                n--;
-            }
+        while(i >= 0 && j >= 0){
+           if(s[j] >= g[i]){
+             ans++;
+             i--;
+             j--;
+           }else i--;
         }
        return ans;
     }
