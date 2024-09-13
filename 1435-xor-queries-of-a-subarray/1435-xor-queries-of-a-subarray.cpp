c3e760aacb6da3d57b171d22583f6aab00d35ab1
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-09-13 09:59:57 +0530

    Time: 51 ms (86.85%), Space: 42.5 MB (34.62%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> ans;
-
+        vector<int> xorArr;
+        int currXOR = 0;
+        for(int i=0 ; i<arr.size(); i++){
+            currXOR ^= arr[i];
+            xorArr.push_back(currXOR);
+        }
+     
         for(auto & q : queries){
             if(q[0] == q[1]){
-                 ans.push_back(arr[q[0]]);
-                 continue;
+                ans.push_back(arr[q[0]]);
             }
-            int currAns = 0;
-            for(int i=q[0] ; i<=q[1]; i++){
-               currAns ^= arr[i];
-            }
-            ans.push_back(currAns);
+            else if(q[0] == 0) ans.push_back(xorArr[q[1]]);
+            else
+            ans.push_back(xorArr[q[0] - 1] ^ xorArr[q[1]]);
         }
-
         return ans;
     }
 };
