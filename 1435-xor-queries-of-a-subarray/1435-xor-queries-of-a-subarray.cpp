e0bfc3d36135ab83c4d487fd7dfd1f4c450b480f
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-09-13 09:42:23 +0530

    Time: 1951 ms (5.03%), Space: 41.7 MB (72.15%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -4,6 +4,10 @@ public:
         vector<int> ans;
 
         for(auto & q : queries){
+            if(q[0] == q[1]){
+                 ans.push_back(arr[q[0]]);
+                 continue;
+            }
             int currAns = 0;
             for(int i=q[0] ; i<=q[1]; i++){
                currAns ^= arr[i];
