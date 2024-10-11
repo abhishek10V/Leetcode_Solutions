8a19b167d192ad99799b230487fef37d7066eb77
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-10-11 16:31:49 +0530

    Time: 124 ms (95.53%), Space: 58.2 MB (86.6%) - LeetHub

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    typedef pair<int, int> p;
+    priority_queue<p, vector<p>, greater<p>> pq;
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int n = times.size();
+        
+        int targetArrivalTime = times[targetFriend][0];
+        
+        sort(begin(times), end(times));
+        
+        set<int> chairNum; //ordered set (we will get the least available chair number in the beginning) (You could also use min heap for this also)
+        
+        int lastChair = 0;
+        
+        for(vector<int> &time : times) {
+            int arrival  = time[0];
+            int depart   = time[1];
+            int currSeat = -1;
+ 
+            while(!pq.empty() && pq.top().first <= arrival) {
+                chairNum.insert(pq.top().second); //insert the chairs which are being emptied
+                pq.pop();
+            }
+
+            if(chairNum.empty()) {
+                currSeat = lastChair;
+                lastChair++;
+            } else {
+                currSeat = *(chairNum.begin());  //ordered set (we will get the least available chair number in the beginning)
+                chairNum.erase(currSeat); //this chair is now occupied
+            }
+            pq.push({depart, currSeat});
+            if(arrival == targetArrivalTime)
+                return currSeat;
+        }
+        
+        return -1;
+    }
+};
