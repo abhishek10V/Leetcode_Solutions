05fb06a2b6a96d8567cba08a71d44ba046393100
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-10-19 21:25:45 +0530

    Time: 0 ms (100%), Space: 7.5 MB (64.16%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        string s = "0";
-        if(k == 1) return '0';
-        n--;
-        while(n-- > 0){
-            string inv = inverse(s);
-           
-            s = s + "1" + reverseString(inv);
-            
-        }
-        return s[k-1];
+    if (n == 1) {
+        return '0';
     }
-    string reverseString(string & s){
-        int l = 0 , r = s.length() - 1;
-        while(l < r){
-            char t = s[l];
-            s[l] = s[r];
-            s[r] = t;
+    
+    int len = (1 << n) - 1; 
+    int mid = len / 2 + 1;  
+    
+    if (k == mid) {
+        return '1';
+    }
+    
+    if (k < mid) {
+        return findKthBit(n - 1, k);
+    }
+    
+    char bit = findKthBit(n - 1, len - k + 1);
+    return bit == '0' ? '1' : '0';
 
-            l++, r--;
-        }
-        return s;
-    }
-    string inverse(string & s){
-        string inv = s;
-        for(int i=0 ; i<inv.length(); i++){
-            if(inv[i] == '0') inv[i] = '1';
-            else inv[i] = '0';
-        }
-        return inv;
     }
 };
