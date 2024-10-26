e72f20e0e077e390a19991118a00a7f0ead6b757
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-10-26 15:12:54 +0530

    Time: 16 ms (86.32%), Space: 69.8 MB (91.23%) - LeetHub

---
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.java
@@ -0,0 +1,42 @@
+class Solution {
+    private int[] level = new int[1000001]; // Level of each node
+    private int[] height = new int[100001]; // Height of each node
+    private int[] levelMaxHt = new int[100001];
+    private int[] levelSecondMaxHt = new int[100001];
+
+    // Method to find the height of each node and populate the max heights at each level
+    private int findHeight(TreeNode root, int l) {
+        if (root == null) {
+            return 0;
+        }
+
+        level[root.val] = l;
+        height[root.val] = Math.max(findHeight(root.left, l + 1), findHeight(root.right, l + 1)) + 1;
+
+        if (levelMaxHt[l] < height[root.val]) {
+            levelSecondMaxHt[l] = levelMaxHt[l];
+            levelMaxHt[l] = height[root.val];
+        } else if (levelSecondMaxHt[l] < height[root.val]) {
+            levelSecondMaxHt[l] = height[root.val];
+        }
+
+        return height[root.val];
+    }
+
+    // Main method to process queries
+    public int[] treeQueries(TreeNode root, int[] queries) {
+        findHeight(root, 0);
+
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int node = queries[i];
+            int L = level[node]; // Node's level
+
+            // Calculate result for this node
+            int tempResult = L + (levelMaxHt[L] == height[node] ? levelSecondMaxHt[L] : levelMaxHt[L]) - 1;
+            result[i] = tempResult;
+        }
+
+        return result;
+    }
+}
