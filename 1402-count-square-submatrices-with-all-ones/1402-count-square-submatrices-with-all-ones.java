c803a79bebc69545bbfe26db94cd020e7602d551
Abhishek Varshney <123306326+abhishek10V@users.noreply.github.com>
2024-10-27 14:41:30 +0530

    Time: 7 ms (45.12%), Space: 55.2 MB (34.04%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -3,29 +3,19 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int[][] dp = new int[m+1][n+1];
+        int[][] dp = new int[m][n];
         int res = 0;
-        
-        for(int[] row : dp) Arrays.fill(row , -1);
 
         for(int i=0 ; i<m; i++){
             for(int j=0 ; j<n ; j++){
-               res += solve(i , j , matrix , dp);
+                if(i == 0 || j == 0) dp[i][j] = matrix[i][j];
+                else if(matrix[i][j] == 1){
+                    dp[i][j] = 1 + Math.min(dp[i-1][j] , Math.min(dp[i-1][j-1] , dp[i][j-1]));
+                }
+
+                res += dp[i][j];
             }
         }
         return res;
-    }
-    public int solve(int i , int j , int[][] matrix, int[][] dp){
-        if(i >= matrix.length || j >= matrix[0].length) return 0;
-
-        if(matrix[i][j] == 0) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(i , j+1 , matrix, dp);
-        int left = solve(i+1 , j , matrix, dp);
-        int diagonal = solve(i+1 , j+1 , matrix, dp);
-        
-        return dp[i][j] = 1 + Math.min(right , Math.min(left , diagonal));
     }
 }
